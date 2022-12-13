26148e3c4830d7ebc57d4d111d3b52f7c1867c60
kerkykerky <77633277+kerkykerky@users.noreply.github.com>
2022-12-14 00:36:37 +0100

    Add files via upload
    ss

---
+++ activeMQ-maven/src/main/java/com/example/activemqmaven/ActiveMqMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.activemqmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqMavenApplication.class, args);
+    }
+
+}
---
+++ activeMQ-maven/src/test/java/com/example/activemqmaven/ActiveMqMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activemqmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
